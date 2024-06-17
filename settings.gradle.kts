2e2278ba806da5628b891740a8b38e7c1847badc
HollowHorizon <artemm@xaker.ru>
2024-06-17 22:01:02 +0300

    futura

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 pluginManagement {
     repositories {
         mavenCentral()
+        maven("https://nexus.gtnewhorizons.com/repository/public/")
         maven("https://maven.minecraftforge.net/")
         maven("https://repo.spongepowered.org/repository/maven-public/")
     }
@@ -10,4 +11,9 @@ pluginManagement {
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version "1.9.22"
     }
+}
+
+plugins {
+    // Automatic toolchain provisioning
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
