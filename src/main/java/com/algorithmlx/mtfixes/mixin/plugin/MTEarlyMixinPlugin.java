b234e45d44f6fcd120383a10009b58e9adc1288c
algorithmlx <izhukov2007@gmail.com>
2024-06-18 02:00:58 +0700

    disabled mixins

--- src/main/java/com/algorithmlx/mtfixes/mixin/plugin/MTEarlyMixinPlugin.java
+++ src/main/java/com/algorithmlx/mtfixes/mixin/plugin/MTEarlyMixinPlugin.java
@@ -1,13 +1,13 @@
-package com.algorithmlx.mtfixes.mixin.plugin;
-
-import com.google.common.collect.Lists;
-import zone.rong.mixinbooter.IEarlyMixinLoader;
-
-import java.util.List;
-
-public class MTEarlyMixinPlugin implements IEarlyMixinLoader {
-    @Override
-    public List<String> getMixinConfigs() {
-        return Lists.newArrayList("mtearly.mixin.json");
-    }
-}
+//package com.algorithmlx.mtfixes.mixin.plugin;
+//
+//import com.google.common.collect.Lists;
+//import zone.rong.mixinbooter.IEarlyMixinLoader;
+//
+//import java.util.List;
+//
+//public class MTEarlyMixinPlugin implements IEarlyMixinLoader {
+//    @Override
+//    public List<String> getMixinConfigs() {
+//        return Lists.newArrayList("mtearly.mixin.json");
+//    }
+//}
--- src/main/java/com/algorithmlx/mtfixes/mixin/plugin/MTLateMixinPlugin.java
+++ src/main/java/com/algorithmlx/mtfixes/mixin/plugin/MTLateMixinPlugin.java
@@ -1,13 +1,13 @@
-package com.algorithmlx.mtfixes.mixin.plugin;
-
-import com.google.common.collect.Lists;
-import zone.rong.mixinbooter.ILateMixinLoader;
-
-import java.util.List;
-
-public class MTLateMixinPlugin implements ILateMixinLoader {
-    @Override
-    public List<String> getMixinConfigs() {
-        return Lists.newArrayList("mtlate.mixins.json");
-    }
-}
+//package com.algorithmlx.mtfixes.mixin.plugin;
+//
+//import com.google.common.collect.Lists;
+//import zone.rong.mixinbooter.ILateMixinLoader;
+//
+//import java.util.List;
+//
+//public class MTLateMixinPlugin implements ILateMixinLoader {
+//    @Override
+//    public List<String> getMixinConfigs() {
+//        return Lists.newArrayList("mtlate.mixins.json");
+//    }
+//}
